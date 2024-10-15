69b0b498b052891a331c2b01a551a3f6facdb3ef
Theodor Radulescu <theodorradulescu123@gmail.com>
2024-10-15 16:21:18 +0300

    Seminar3

---
+++ app/src/main/java/com/example/seminar3/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.seminar3;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+
+
+    }
+    public void deschide(View view) {
+        Intent it = new Intent(getApplicationContext(), MainActivity2.class);
+        it.putExtra("text","Acesta este un text transmis din prima activitate");
+        it.putExtra("numar1",3);
+        it.putExtra("numarul2",18);
+        startActivity(it);
+    }
+}
